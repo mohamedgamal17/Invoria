24c2e1cdfeb094bf400de9f81ed4e4759a15bea1
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-15 20:03:53 +0200

    feat(Id-generator): Add custom EFCore Ulid generator

---
+++ src/BuildingBlocks/Invoria.BuildingBlocks.EntityFramework/Extensions/EntityConfigurationBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿
+using Invoria.BuildingBlocks.Domain.Entities;
+using Invoria.BuildingBlocks.EntityFramework.Generators;
+using Invoria.BuildingBlocks.EntityFramework.Primitives;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Invoria.BuildingBlocks.EntityFramework.Extensions
+{
+    public static class EntityConfigurationBuilderExtensions
+    {
+        public static void MapAudited<T>(this EntityTypeBuilder<T> builder) 
+            where  T : class , IAuditedEntity 
+           
+        {
+            builder.Property(x => x.CreatedBy).HasMaxLength(256);
+            builder.Property(x => x.LastModifiedBy).IsRequired(false).HasMaxLength(256);
+        }
+
+        public static void MapId<T>(this EntityTypeBuilder<T>  builder)
+            where T : class , IEntity
+        {
+            builder.Property(x => x.Id);
+
+            builder.Property(x => x.Id).HasMaxLength(256).HasValueGenerator<UlidStringValueGenerator>();
+        }
+    }
+}
---
+++ src/BuildingBlocks/Invoria.BuildingBlocks.EntityFramework/Generators/UlidStringValueGenerator.cs
@@ -0,0 +1,15 @@
+﻿
+using Invoria.BuildingBlocks.EntityFramework.Primitives;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+
+namespace Invoria.BuildingBlocks.EntityFramework.Generators
+{
+    public class UlidStringValueGenerator : ValueGenerator<string>
+    {
+        public override bool GeneratesTemporaryValues => false;
+
+        public override string Next(EntityEntry entry)
+            => UlidGenerator.NewUlid().ToString();
+    }
+}
