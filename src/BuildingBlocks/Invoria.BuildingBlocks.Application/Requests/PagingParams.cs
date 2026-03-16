83657781ebccc6aea79f0aa89167c1adc2850de4
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-17 01:34:04 +0200

    feat(paging): Add global paging params class

---
+++ src/BuildingBlocks/Invoria.BuildingBlocks.Application/Requests/PagingParams.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace Invoria.BuildingBlocks.Application.Requests
+{
+    public class PagingParams
+    {
+        public int Skip { get; set; } = 0;
+        public int Length { get; set; } = 10;
+    }
+
+    public class PagingParamasValidator<T> : AbstractValidator<PagingParams> where T : PagingParams
+    {
+        public PagingParamasValidator()
+        {
+            RuleFor(x => x.Skip)
+                .GreaterThanOrEqualTo(0);
+
+            RuleFor(x => x.Length)
+                .GreaterThan(0)
+                .LessThanOrEqualTo(100);
+
+        }
+    }
+}
