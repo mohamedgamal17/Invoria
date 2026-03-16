3cfbaca11611f3e128805b16b58e7417e51e12c6
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-16 22:51:27 +0200

    feat(service-collection): Add additional service collection extensions helper methods - Add Replace method to be able to replace current service implementation with another service by passing it's type -  Add GetSinglatonOrNull to get the current or default instance of specifi time before DI container initialization

---
+++ src/BuildingBlocks/Invoria.BuildingBlocks.Core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+namespace Invoria.BuildingBlocks.Core.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection Replace<TService, TImplementaion>(this IServiceCollection services)
+        {
+            return services.Replace(typeof(TService), typeof(TImplementaion));
+        }
+
+        public static IServiceCollection Replace(this IServiceCollection services, Type service, Type implementaion)
+        {
+            var oldService = services.SingleOrDefault(x => x.ServiceType == service);
+
+            if (oldService != null)
+            {
+                services.Remove(oldService);
+
+            }
+
+            var serviceDescriptor = new ServiceDescriptor(service, implementaion, oldService?.Lifetime ?? ServiceLifetime.Transient);
+
+            services.Add(serviceDescriptor);
+
+            return services;
+        }
+
+        public static T? GetSinglatonOrNull<T>(this IServiceCollection services)
+        {
+            return (T)services.GetSinglatonOrNull(typeof(T));
+        }
+        public static object? GetSinglatonOrNull(this IServiceCollection services, Type targetType)
+        {
+            var descriptor = services.FirstOrDefault(
+                d => d.ServiceType == targetType && d.Lifetime == ServiceLifetime.Singleton);
+
+            return descriptor?.ImplementationInstance;
+        }
+    }
+}
