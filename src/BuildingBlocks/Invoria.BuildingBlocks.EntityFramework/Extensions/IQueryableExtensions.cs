9ea1c466f7d37f831c02b1f8a99080e0fbb92d8d
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-17 01:32:53 +0200

    feat(paging): Add EfCore paging extension method

---
+++ src/BuildingBlocks/Invoria.BuildingBlocks.EntityFramework/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Invoria.BuildingBlocks.Domain.Dtos;
+using Microsoft.EntityFrameworkCore;
+
+namespace Invoria.BuildingBlocks.EntityFramework.Extensions
+{
+    public static class IQueryableExtensions
+    {
+        public static async Task<PagingDto<T>> ToPaged<T>(this IQueryable<T> query, int skip, int length)
+        {
+            var result = await query.Skip(skip).Take(length).ToListAsync();
+
+            var count = await query.CountAsync();
+
+            var info = new PagingInfoDto()
+            {
+                Skip = skip,
+                Length = length,
+                TotalCount = count
+            };
+
+
+            var paging = new PagingDto<T>()
+            {
+                Data = result,
+                Info = info
+            };
+            return paging;
+        }
+
+    }
+}
