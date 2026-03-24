61da2cede4a22b70b750d6b96351fb4193e66c49
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-25 01:28:08 +0200

    feat(UpdateOrderCommand): Add base UpdateOrderCommand for updating order items

---
+++ src/Modules/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -0,0 +1,17 @@
+using Invoria.BuildingBlocks.Application.Abstractions.Cqrs;
+using Invoria.Ordering.Application.Orders.Commands.CreateOrder;
+using Invoria.Ordering.Contracts.Dtos;
+
+namespace Invoria.Ordering.Application.Orders.Commands.UpdateOrder;
+
+public class UpdateOrderCommand : ICommand<OrderDto>
+{
+    public string Id { get; set; }
+    public List<CreateOrderItemCommand> Items { get; set; }
+
+    public UpdateOrderCommand(string id, List<CreateOrderItemCommand> items)
+    {
+        Id = id;
+        Items = items;
+    }
+}
