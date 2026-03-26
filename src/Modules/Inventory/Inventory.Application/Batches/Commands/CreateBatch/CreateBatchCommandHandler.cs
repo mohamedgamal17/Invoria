cc039838116e700088770b0791a32bd0b34db76a
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-26 21:21:44 +0200

    feat(inventory): Add CreateBatchCommandHandler to handle CreateBatchCommand

---
+++ src/Modules/Inventory/Inventory.Application/Batches/Commands/CreateBatch/CreateBatchCommandHandler.cs
@@ -0,0 +1,33 @@
+using Invoria.BuildingBlocks.Application.Abstractions.Cqrs;
+using Invoria.BuildingBlocks.Domain.Primitives;
+using Invoria.Inventory.Application.Batches.Factories;
+using Invoria.Inventory.Contracts.Dtos;
+using Invoria.Inventory.Domain;
+using Invoria.Inventory.Domain.Batches;
+
+namespace Invoria.Inventory.Application.Batches.Commands.CreateBatch;
+
+public class CreateBatchCommandHandler : IApplicatonRequestHandler<CreateBatchCommand, BatchDto>
+{
+    private readonly IInventoryRepository<Batch> _batchRepository;
+    private readonly IBatchResponseFactory _batchResponseFactory;
+
+    public CreateBatchCommandHandler(
+        IInventoryRepository<Batch> batchRepository,
+        IBatchResponseFactory batchResponseFactory)
+    {
+        _batchRepository = batchRepository;
+        _batchResponseFactory = batchResponseFactory;
+    }
+
+    public async Task<Result<BatchDto>> Handle(CreateBatchCommand request, CancellationToken cancellationToken)
+    {
+        var batch = new Batch(request.ProductId, request.Quantity,  request.PurchasePrice);
+
+        await _batchRepository.Add(batch, cancellationToken);
+
+        var dto = await _batchResponseFactory.PrepareDto(batch);
+
+        return dto;
+    }
+}
