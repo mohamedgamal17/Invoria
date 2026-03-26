de1ab990fb5d2ace18584479a38a2afde129b4fd
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-26 21:19:50 +0200

    feat(inventory): Add BatchDto api response

---
+++ src/Modules/Inventory/Inventory.Contracts/Dtos/BatchDto.cs
@@ -0,0 +1,11 @@
+using Invoria.BuildingBlocks.Domain.Dtos;
+
+namespace Invoria.Inventory.Contracts.Dtos;
+
+public class BatchDto : AuditedEntityDto
+{
+    public string ProductId { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+    public int ReservedQuantity { get; set; }
+    public decimal PurchasePrice { get; set; }
+}
