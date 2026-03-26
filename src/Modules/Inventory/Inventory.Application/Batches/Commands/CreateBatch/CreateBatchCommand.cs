3c25fab77a468d4b9a4663a64039a7987f25713f
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-26 21:20:49 +0200

    feat(inventory): Add CreateBatchCommand to create new product batch

---
+++ src/Modules/Inventory/Inventory.Application/Batches/Commands/CreateBatch/CreateBatchCommand.cs
@@ -0,0 +1,18 @@
+using Invoria.BuildingBlocks.Application.Abstractions.Cqrs;
+using Invoria.Inventory.Contracts.Dtos;
+
+namespace Invoria.Inventory.Application.Batches.Commands.CreateBatch;
+
+public class CreateBatchCommand : ICommand<BatchDto>
+{
+    public string ProductId { get; set; }
+    public int Quantity { get; set; }
+    public decimal PurchasePrice { get; set; }
+
+    public CreateBatchCommand(string productId, int quantity, decimal purchasePrice)
+    {
+        ProductId = productId;
+        Quantity = quantity;
+        PurchasePrice = purchasePrice;
+    }
+}
