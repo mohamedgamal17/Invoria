7a466c53df249bccb4d0e41446d4582751cd9f89
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-26 19:56:06 +0200

    feat(inventory): Add Batch Aggregate Root EFcore configuration and migration

---
+++ src/Modules/Inventory/Inventory.Infrastructure/EntityFramework/Configuration/BatchEntityTypeConfiguration.cs
@@ -0,0 +1,30 @@
+using Invoria.BuildingBlocks.EntityFramework.Extensions;
+using Invoria.Inventory.Domain.Batches;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Invoria.Inventory.Infrastructure.EntityFramework.Configuration
+{
+    public class BatchEntityTypeConfiguration : IEntityTypeConfiguration<Batch>
+    {
+        public void Configure(EntityTypeBuilder<Batch> builder)
+        {
+            builder.ToTable(BatchTableConsts.TableName);
+
+            builder.MapId();
+
+            builder.Property(x => x.Id)
+                .HasMaxLength(BatchTableConsts.IdMaxLength);
+
+            builder.Property(x => x.ProductId)
+                .HasMaxLength(BatchTableConsts.ProductIdMaxLength);
+
+            builder.Property(x => x.Quantity);
+            builder.Property(x => x.ReservedQuantity);
+            builder.Property(x => x.PurchasePrice)
+                .HasPrecision(18, 2);
+
+            builder.MapAudited();
+        }
+    }
+}
---
+++ src/Modules/Inventory/Inventory.Infrastructure/EntityFramework/Migrations/20260326174209_InitialInventoryBatchMigration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Invoria.Inventory.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialInventoryBatchMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Batches",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    ProductId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    ReservedQuantity = table.Column<int>(type: "int", nullable: false),
+                    PurchasePrice = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    LastModifiedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Batches", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Batches");
+        }
+    }
+}
