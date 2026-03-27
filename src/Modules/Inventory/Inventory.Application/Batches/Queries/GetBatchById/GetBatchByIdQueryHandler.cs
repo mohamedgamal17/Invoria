66309a32c3a257b9045d0291dda4c6a8a10e6c91
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-27 06:18:22 +0200

    feat(inventory): Add GetBatchByIdQuery handler

---
+++ src/Modules/Inventory/Inventory.Application/Batches/Queries/GetBatchById/GetBatchByIdQueryHandler.cs
@@ -0,0 +1,35 @@
+using Invoria.BuildingBlocks.Application.Abstractions.Cqrs;
+using Invoria.BuildingBlocks.Domain.Exceptions;
+using Invoria.BuildingBlocks.Domain.Primitives;
+using Invoria.Inventory.Application.Batches.Factories;
+using Invoria.Inventory.Contracts.Dtos;
+using Invoria.Inventory.Domain;
+using Invoria.Inventory.Domain.Batches;
+
+namespace Invoria.Inventory.Application.Batches.Queries.GetBatchById;
+
+public class GetBatchByIdQueryHandler : IApplicatonRequestHandler<GetBatchByIdQuery, BatchDto>
+{
+    private readonly IInventoryRepository<Batch> _batchRepository;
+    private readonly IBatchResponseFactory _batchResponseFactory;
+
+    public GetBatchByIdQueryHandler(
+        IInventoryRepository<Batch> batchRepository,
+        IBatchResponseFactory batchResponseFactory)
+    {
+        _batchRepository = batchRepository;
+        _batchResponseFactory = batchResponseFactory;
+    }
+
+    public async Task<Result<BatchDto>> Handle(GetBatchByIdQuery request, CancellationToken cancellationToken)
+    {
+        var batch = await _batchRepository.SingleOrDefault(x => x.Id == request.Id, cancellationToken);
+        if (batch == null)
+        {
+            return Result.Failure<BatchDto>(new NotFoundException($"Batch with ID {request.Id} not found"));
+        }
+
+        var dto = await _batchResponseFactory.PrepareDto(batch);
+        return dto;
+    }
+}
