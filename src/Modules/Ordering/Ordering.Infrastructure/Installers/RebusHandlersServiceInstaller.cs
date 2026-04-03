b345fe8ecf6b35cc7f3df85678dce16a9a43469a
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-04-03 18:49:26 +0200

    feat(messaging): Register OrderAllocationSucceededIntegrationEvent consumer

---
+++ src/Modules/Ordering/Ordering.Infrastructure/Installers/RebusHandlersServiceInstaller.cs
@@ -0,0 +1,16 @@
+using Invoria.BuildingBlocks.Core.Modularity;
+using Invoria.Ordering.Application.Orders.Consumers;
+using Invoria.Ordering.Contracts.Events;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Rebus.Handlers;
+
+namespace Invoria.Ordering.Infrastructure.Installers;
+
+public sealed class RebusHandlersServiceInstaller : IServiceInstaller
+{
+    public void Install(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<IHandleMessages<OrderAllocationSucceededIntegrationEvent>, OrderAllocationSucceededIntegrationEventConsumer>();
+    }
+}
