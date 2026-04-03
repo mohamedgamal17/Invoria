4e5f450178350d9a77805687b68578c9b03dda4c
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-04-03 22:15:57 +0200

    test(order): Add new order complete test cases

---
+++ tests/Modules/Ordering/Ordering.Application.Tests/Domain/Orders/OrderCompleteDomainTests.cs
@@ -0,0 +1,40 @@
+using FluentAssertions;
+using Invoria.BuildingBlocks.Domain.Entities;
+using Invoria.Ordering.Domain.Orders;
+
+namespace Invoria.Ordering.Application.Tests.Domain.Orders;
+
+[TestFixture]
+public class OrderCompleteDomainTests
+{
+    [Test]
+    public void Complete_throws_when_accepted_but_not_dispatched()
+    {
+        var order = new Order("N-C1", "cust");
+        typeof(Entity<string>).GetProperty(nameof(Entity<string>.Id))!.SetValue(order, "order-complete-alloc");
+        order.UpdateItems(new List<OrderItem> { new("p1", 1, 10m) });
+        order.Accept();
+        order.MarkInventoryAllocated();
+
+        var act = () => order.Complete();
+
+        act.Should().Throw<InvalidOperationException>();
+        order.Status.Should().Be(OrderStatus.Accepted);
+    }
+
+    [Test]
+    public void Complete_sets_status_to_completed_when_accepted_and_dispatched()
+    {
+        var order = new Order("N-C2", "cust");
+        typeof(Entity<string>).GetProperty(nameof(Entity<string>.Id))!.SetValue(order, "order-complete-ok");
+        order.UpdateItems(new List<OrderItem> { new("p1", 2, 10m) });
+        order.Accept();
+        order.MarkInventoryAllocated();
+        order.MarkDispatched();
+
+        order.Complete();
+
+        order.Status.Should().Be(OrderStatus.Completed);
+        order.FullfillmentStatus.Should().Be(FullfillmentStatus.Dispatched);
+    }
+}
