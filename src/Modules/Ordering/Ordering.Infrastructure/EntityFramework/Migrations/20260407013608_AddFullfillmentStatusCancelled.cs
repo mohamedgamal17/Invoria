59f076f8f747cc5889588aeb71c4db232a6c252f
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-04-07 04:39:44 +0200

    refactor(order): Reupdate efcore migration to support order refusal process

---
+++ src/Modules/Ordering/Ordering.Infrastructure/EntityFramework/Migrations/20260407013608_AddFullfillmentStatusCancelled.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Invoria.Ordering.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFullfillmentStatusCancelled : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
