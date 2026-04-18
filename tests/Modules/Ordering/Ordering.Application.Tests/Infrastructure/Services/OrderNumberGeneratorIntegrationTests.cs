fec2d72139205d13ee4e386befe693f19d612121
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-04-19 00:59:17 +0200

    test(ordering): Assert the fix of the bug of same ordernumber generatew

--- tests/Modules/Ordering/Ordering.Application.Tests/Infrastructure/Services/OrderNumberGeneratorIntegrationTests.cs
+++ tests/Modules/Ordering/Ordering.Application.Tests/Infrastructure/Services/OrderNumberGeneratorIntegrationTests.cs
@@ -13,12 +13,24 @@ namespace Invoria.Ordering.Application.Tests.Infrastructure.Services
         public async Task Should_generate_unique_order_numbers_under_requests()
         {
             var generator = ServiceProvider.GetRequiredService<IOrderNumberGenerator>();
+            var firstOrderNumber = await generator.GenerateAsync();
+            var secondOrderNumber = await generator.GenerateAsync();
+            var thirdOrderNumber = await generator.GenerateAsync();
 
-            const int total = 200;
+            firstOrderNumber.Length.Should().Be(10);
+            secondOrderNumber.Length.Should().Be(10);
+            thirdOrderNumber.Length.Should().Be(10);
 
-            var orderNumber = await generator.GenerateAsync();
+            secondOrderNumber.Should().NotBe(firstOrderNumber);
+            thirdOrderNumber.Should().NotBe(firstOrderNumber);
+            thirdOrderNumber.Should().NotBe(secondOrderNumber);
 
-            orderNumber.Length.Should().Be(10);
+            var firstSequence = int.Parse(firstOrderNumber[6..]);
+            var secondSequence = int.Parse(secondOrderNumber[6..]);
+            var thirdSequence = int.Parse(thirdOrderNumber[6..]);
+
+            secondSequence.Should().Be(firstSequence + 1);
+            thirdSequence.Should().Be(secondSequence + 1);
         }
     }
 }
