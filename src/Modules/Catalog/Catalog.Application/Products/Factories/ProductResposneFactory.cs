52e14d6cb5d08875dbee7a4e3a4a7e74659b0a6d
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-04-20 09:49:05 +0200

    refactor(product): Refactor ProductResposneFactory to include the ampping stock property into the ProductDto

--- src/Modules/Catalog/Catalog.Application/Products/Factories/ProductResposneFactory.cs
+++ src/Modules/Catalog/Catalog.Application/Products/Factories/ProductResposneFactory.cs
@@ -1,24 +1,61 @@
 using Invoria.BuildingBlocks.Application.Factories;
 using Invoria.Catalog.Contracts.Dtos;
 using Invoria.Catalog.Domain.Products;
+using Invoria.Inventory.Application.Stock;
+using Invoria.Inventory.Contracts.Dtos;
 
 namespace Invoria.Catalog.Application.Products.Factories
 {
     public class ProductResposneFactory : ResponseFactory<Product, ProductDto>, IProductResponseFactory
     {
-        public override Task<ProductDto> PrepareDto(Product view)
+        private readonly IProductStockService _productStockService;
+
+        public ProductResposneFactory(IProductStockService productStockService)
+        {
+            _productStockService = productStockService;
+        }
+
+        public override async Task<ProductDto> PrepareDto(Product view)
+        {
+            var stock = await _productStockService.GetProductStock(view.Id).ConfigureAwait(false);
+            return CreateProductDto(view, stock);
+        }
+
+        public override async Task<List<ProductDto>> PrepareListDto(List<Product> views)
+        {
+            if (views.Count == 0)
+            {
+                return new List<ProductDto>();
+            }
+
+            var productIds = views.Select(x => x.Id).Distinct().ToList();
+            var stockMap = await _productStockService.GetListProductsStock(productIds).ConfigureAwait(false);
+
+            var dtos = new List<ProductDto>(views.Count);
+            foreach (var view in views)
+            {
+                var stock = stockMap.TryGetValue(view.Id, out var mappedStock)
+                    ? mappedStock
+                    : new StockDto();
+                dtos.Add(CreateProductDto(view, stock));
+            }
+
+            return dtos;
+        }
+
+        private ProductDto CreateProductDto(Product view, StockDto stock)
         {
             var dto = new ProductDto
             {
                 Id = view.Id,
                 Name = view.Name,
                 Code = view.Code,
-                Price = view.Price
+                Price = view.Price,
+                Stock = stock
             };
 
             MapAudited(view, dto);
-
-            return Task.FromResult(dto);
+            return dto;
         }
     }
 }
