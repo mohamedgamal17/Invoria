6ba4868e4cc6af9dd6483e73170622aa22687191
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-04-20 09:48:22 +0200

    feat(product): Include StockDto property into ProductDto class

--- src/Modules/Catalog/Catalog.Contracts/Dtos/ProductDto.cs
+++ src/Modules/Catalog/Catalog.Contracts/Dtos/ProductDto.cs
@@ -1,5 +1,6 @@
 using Invoria.BuildingBlocks.Domain.Dtos;
 using Invoria.BuildingBlocks.Domain.Entities;
+using Invoria.Inventory.Contracts.Dtos;
 
 namespace Invoria.Catalog.Contracts.Dtos
 {
@@ -8,5 +9,6 @@ namespace Invoria.Catalog.Contracts.Dtos
         public string Name { get; set; }
         public string? Code { get; set; }
         public decimal Price { get; set; }
+        public StockDto Stock { get; set; } = new();
     }
 }
