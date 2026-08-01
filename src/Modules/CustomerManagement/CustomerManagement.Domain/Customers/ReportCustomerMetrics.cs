c71d2c342a2c7aa1cb58eee016fddc3ec6cd91ed
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-08-01 22:53:50 +0300

    feat: add ReportCustomerMetrics report class to CustomerManagement.Domain

---
+++ src/Modules/CustomerManagement/CustomerManagement.Domain/Customers/ReportCustomerMetrics.cs
@@ -0,0 +1,19 @@
+using Invoria.BuildingBlocks.Domain.Entities;
+using Invoria.BuildingBlocks.Domain.Enums;
+
+namespace Invoria.CustomerManagement.Domain.Customers
+{
+    public class ReportCustomerMetrics : Entity
+    {
+        public long TotalCount { get; private set; }
+        public ReportPeriod Period { get; private set; }
+
+        private ReportCustomerMetrics() { }
+
+        public ReportCustomerMetrics(long totalCount, ReportPeriod period)
+        {
+            TotalCount = totalCount;
+            Period = period;
+        }
+    }
+}
