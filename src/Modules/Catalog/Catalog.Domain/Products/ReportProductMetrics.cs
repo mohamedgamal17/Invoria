ef4b503e18ee6ac70cb24a3435ef0c647c4a68e8
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-08-02 02:59:47 +0300

    feat: add Date to report metrics entities

--- src/Modules/Catalog/Catalog.Domain/Products/ReportProductMetrics.cs
+++ src/Modules/Catalog/Catalog.Domain/Products/ReportProductMetrics.cs
@@ -5,13 +5,15 @@ namespace Invoria.Catalog.Domain.Products
 {
     public class ReportProductMetrics : Entity
     {
+        public DateTimeOffset Date { get; private set; }
         public long TotalCount { get; private set; }
         public ReportPeriod Period { get; private set; }
 
         private ReportProductMetrics() { }
 
-        public ReportProductMetrics(long totalCount, ReportPeriod period)
+        public ReportProductMetrics(DateTimeOffset date, long totalCount, ReportPeriod period)
         {
+            Date = date;
             TotalCount = totalCount;
             Period = period;
         }
--- src/Modules/CustomerManagement/CustomerManagement.Domain/Customers/ReportCustomerMetrics.cs
+++ src/Modules/CustomerManagement/CustomerManagement.Domain/Customers/ReportCustomerMetrics.cs
@@ -5,13 +5,15 @@ namespace Invoria.CustomerManagement.Domain.Customers
 {
     public class ReportCustomerMetrics : Entity
     {
+        public DateTimeOffset Date { get; private set; }
         public long TotalCount { get; private set; }
         public ReportPeriod Period { get; private set; }
 
         private ReportCustomerMetrics() { }
 
-        public ReportCustomerMetrics(long totalCount, ReportPeriod period)
+        public ReportCustomerMetrics(DateTimeOffset date, long totalCount, ReportPeriod period)
         {
+            Date = date;
             TotalCount = totalCount;
             Period = period;
         }
