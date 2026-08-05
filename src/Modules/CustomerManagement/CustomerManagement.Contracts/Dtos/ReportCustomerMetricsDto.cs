3f895648d1f927d1deb8890c5769c674b4704cdf
MohamedGamal <mohamedsherf17@outlook.com>
2026-08-05 09:19:43 +0300

    feat: update report customer metrics DTO in Contracts layer

--- src/Modules/CustomerManagement/CustomerManagement.Contracts/Dtos/ReportCustomerMetricsDto.cs
+++ src/Modules/CustomerManagement/CustomerManagement.Contracts/Dtos/ReportCustomerMetricsDto.cs
@@ -2,9 +2,9 @@ namespace Invoria.CustomerManagement.Contracts.Dtos
 {
     public class ReportCustomerMetricsDto
     {
-        public ReportCustomerMetricsPeriodDto? ThisDay { get; set; }
-        public ReportCustomerMetricsPeriodDto? ThisMonth { get; set; }
-        public ReportCustomerMetricsPeriodDto? ThisYear { get; set; }
-        public ReportCustomerMetricsPeriodDto? AllTime { get; set; }
+        public ReportCustomerMetricsPeriodDto ThisDay { get; set; } = new();
+        public ReportCustomerMetricsPeriodDto ThisMonth { get; set; } = new();
+        public ReportCustomerMetricsPeriodDto ThisYear { get; set; } = new();
+        public ReportCustomerMetricsPeriodDto AllTime { get; set; } = new();
     }
 }
