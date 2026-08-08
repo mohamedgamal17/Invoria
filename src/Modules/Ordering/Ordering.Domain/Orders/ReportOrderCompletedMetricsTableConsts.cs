ea5135ec0d16d3f5cac977c9668564176ebe9c0a
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-08-08 18:28:01 +0300

    feat: add ReportOrderCompletedMetrics entity in Domain layer

---
+++ src/Modules/Ordering/Ordering.Domain/Orders/ReportOrderCompletedMetricsTableConsts.cs
@@ -0,0 +1,8 @@
+namespace Invoria.Ordering.Domain.Orders;
+
+public static class ReportOrderCompletedMetricsTableConsts
+{
+    public const string TableName = "ReportOrderCompletedMetrics";
+
+    public const int IdMaxLength = 256;
+}
